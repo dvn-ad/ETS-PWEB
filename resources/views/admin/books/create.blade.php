afcf4006e8601b1e4b0a434ea61c2d9466565a49
Davin Adiputra Suryolaksana <davin.adisuryo@gmail.com>
2025-10-22 14:19:28 +0700

    Update create.blade.php

--- resources/views/admin/books/create.blade.php
+++ resources/views/admin/books/create.blade.php
@@ -1,3 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <!-- link style.css -->
+    <link href="https://ets-pweb-production.up.railway.app/style.css" rel="stylesheet" type="text/css"> 
+</head>
 <h1>Create Book</h1>
 
 <p><a href="{{ route('books.index') }}">← Back</a></p>
